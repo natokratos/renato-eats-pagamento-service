cd977657b91aefda8398e5f687e77fc180921e4a
natokratos <micro8696@caelum114-04>
2019-10-28 12:54:18 -0300

    exercicios

---
+++ src/test/java/br/com/caelum/eatspagamento/EatsPagamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caelum.eatspagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatsPagamentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
